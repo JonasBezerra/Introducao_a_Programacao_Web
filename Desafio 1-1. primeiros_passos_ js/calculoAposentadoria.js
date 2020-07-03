6ec066198de70e84913fe8b68189449666e9bd21
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-03 19:04:07 -0400

    Create calculoAposentadoria.js

---
+++ Desafio 1-1. primeiros_passos_ js/calculoAposentadoria.js
@@ -0,0 +1,12 @@
+const nome = "Silvana";
+const sexo = "F";
+const idade = 48;
+const contribuicao = 23;
+
+const totalContribuicao = contribuicao + idade
+
+if (totalContribuicao) {
+    
+} else {
+    
+}
