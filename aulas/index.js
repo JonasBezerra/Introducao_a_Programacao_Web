8ee1fa56a150aead4a27df76be4013ebf37284c4
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-03 17:31:16 -0400

    Update index.js

--- aulas/index.js
+++ aulas/index.js
@@ -2,6 +2,7 @@
 // das notas entre os alunos e envia
 // mensagem do calculo da média
 
+
 const aluno01 = "Jonas"
 const notaAluno01 = 9.8
 
@@ -15,12 +16,14 @@ const media = (notaAluno01 + notaAluno02 + notaAluno03)/3
 
 // se a média for maior que 5 parabenizar a turma
 
-if(media > 5){
-    console.log(`A nota foi  de ${media}. Parabéns a Turma!.`)
+if(media > 5){ // true
+    // Faz alguma Coisa
+    console.log(`A nota foi  de ${media.toFixed(1)} Parabéns a Turma!.`)
 }else{
-    console.log(``)
+    // Faz putra Coisa
+    console.log(`A Nota da turma foi: ${media}média foi Menor que 5`)
 }
-console.log(`A Nota dos Alunos é ${media}`);
+
 
 
 
