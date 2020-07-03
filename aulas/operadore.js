df8784736d302ec7da2b7a4da9e5adb32f9832ba
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-03 17:36:53 -0400

    Create operadore.js

---
+++ aulas/operadore.js
@@ -0,0 +1,14 @@
+/* ==========================================
+    OPERADORES DE COMPARAÇÃO
+    
+    >   MAIOR
+    <   MENOR
+    >=  MAIOR IGUAL A
+    <=  MENOR IGUAL A
+    ==  IGUAL A
+    === IGUAL E DO MESMO TIPO
+    !=  DIFERENTE DE
+    !== DIFERENTE, INCLUSIVE DO TIPO
+==============================================*/
+
+console(5 > 4) // true; false ()
