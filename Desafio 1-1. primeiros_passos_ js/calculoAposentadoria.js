b3d332389c8ed57f7abea11e0f3631aacb36365e
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-03 19:04:29 -0400

    Update calculoAposentadoria.js

--- Desafio 1-1. primeiros_passos_ js/calculoAposentadoria.js
+++ Desafio 1-1. primeiros_passos_ js/calculoAposentadoria.js
@@ -1,12 +1,16 @@
-const nome = "Silvana";
-const sexo = "F";
-const idade = 48;
-const contribuicao = 23;
+const nome = 'Silvana'
+const sexo = 'F'
+const idade = 48
+const contribuicao = 23
 
-const totalContribuicao = contribuicao + idade
+const calculoContribuicao = idade + contribuicao
 
-if (totalContribuicao) {
-    
+// essas variáveis irão retornar true ou false
+const homemPodeAposentar = sexo == 'M' && contribuicao >= 35 && calculoContribuicao >= 95
+const mulherPodeAposentar = sexo == 'F' && contribuicao >= 30 && calculoContribuicao >= 85
+
+if ( homemPodeAposentar || mulherPodeAposentar) {
+    console.log(`${nome}, você pode se aposentar!`)
 } else {
-    
+    console.log(`${nome}, você não pode se aposentar!`)
 }
