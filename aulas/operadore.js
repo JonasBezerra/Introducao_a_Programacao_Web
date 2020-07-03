77170eb947f9d1fd8acc92cf3ac78c427614e967
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-03 17:43:30 -0400

    Update operadore.js

--- aulas/operadore.js
+++ aulas/operadore.js
@@ -11,4 +11,13 @@
     !== DIFERENTE, INCLUSIVE DO TIPO
 ==============================================*/
 
-console(5 > 4) // true; false ()
+console.log( 5 > 4 )  // true
+console.log( 5< 4 )   // false
+console.log( 5 >= 4 ) // true
+console.log( 4 <= 4 ) // true
+
+console.log('\n================================\n')
+console.log( 4 == "4" )   // true
+console.log( 4 === "4" )  // false.
+console.log( 4 != "5"); // true
+console.log( 4 !== "5") // true
