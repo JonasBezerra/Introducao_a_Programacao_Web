eeec275c76574da899899017f0ba3a7388d0bdb8
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-03 18:29:25 -0400

    Update operadoresLogicos.js

--- aulas/operadoresLogicos.js
+++ aulas/operadoresLogicos.js
@@ -8,3 +8,20 @@
     !   "NÃO" Nega uma condição    
 
 =====================================================*/
+
+console.log(" ============== && ========= \n")
+console.log( 5 == 5 && 6 == 6 ) // true
+console.log( 5 == 5 && 2 == 6 ) // false
+console.log( 5 == 5 && 6 == 7 ) // false
+
+console.log("\n ============== OU ========= \n")
+console.log( 5 == 5 || 6 == 6 ) // true
+console.log( 5 == 5 || 5 == 6 ) // true
+console.log( 5 == 5 || 6 == 7 ) // true
+
+console.log("\n ============== ! ========= \n")
+console.log( !(5 == 5) ) // false
+console.log( !(5 != 5) ) // true
+
+
+
