7d97180553dd8052642181ad1bcb3663eab7cb58
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-03 18:00:40 -0400

    Update operadore.js

--- aulas/operadore.js
+++ aulas/operadore.js
@@ -9,7 +9,9 @@
     === IGUAL E DO MESMO TIPO
     !=  DIFERENTE DE
     !== DIFERENTE, INCLUSIVE DO TIPO
+
 ==============================================*/
+
 
 console.log( 5 > 4 )  // true
 console.log( 5< 4 )   // false
@@ -21,3 +23,22 @@ console.log( 4 == "4" )   // true
 console.log( 4 === "4" )  // false.
 console.log( 4 != "5"); // true
 console.log( 4 !== "5") // true
+
+// DESAFIO 1
+// verificar se a pessoa é maior igual de 18 anos
+// se sim, deixar entrar, senão, bloquear a entrada
+
+const idade = 17
+
+if (idade >= 18) {
+    console.log(`\nDeixar entrar`)
+} else {
+    console.log(`\nBloquear Entrada!.`)
+}
+
+// se a pessoa tiver 17 anos
+// avisar para voltar quando fizer 18 anos
+
+if(idade == 17){
+    console.log(`Volte quando fizer ${idade} anos!.\n`)
+}
