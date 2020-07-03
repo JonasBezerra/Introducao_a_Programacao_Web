294e67cd62ddb42333662c61cb13c136292b67f7
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-03 18:17:24 -0400

    Create operadoresLogicos.js

---
+++ aulas/operadoresLogicos.js
@@ -0,0 +1,10 @@
+/* ====================================================
+    OPERADORES LÓGICOS
+
+    &&  "E" As duas condições devem ser verdadeiros 
+        para que a condição final seja verdadeira.
+    || "OU" Uma das condições deve ser verdadeira 
+        para que a condição final seja verdadeira.
+    !   "NÃO" Nega uma condição    
+
+=====================================================*/
