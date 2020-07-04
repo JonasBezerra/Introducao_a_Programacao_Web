57d3e5e9366b049fb6aaa0cdb3a0cf5d16993b1c
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-04 17:05:33 -0400

    Update funcoesEmetodos.js

--- aulas/funcoesEmetodos.js
+++ aulas/funcoesEmetodos.js
@@ -12,10 +12,27 @@ const alunosTurma1 = [
 const alunosTurma2 = [
     {nome: 'Robson', nota: 10},
     {nome:'Lucy', nota:9.8},
-    {nome:'Ciclano', nota:5}
+    {nome:'Siclano', nota:2}
     
 ]
 
-function calcularMedia(alunos) {
-    const media = (alunosTurma1[0].nota + alunosTurma1[1].nota + alunosTurma1[2].nota)/3
+
+
+const mediaTurma1 = calcularMedia(alunosTurma1);
+const mediaTurma2 = calcularMedia(alunosTurma2);
+
+enviaMsg(mediaTurma1,'Turma A');
+enviaMsg(mediaTurma2,'Turma B')
+
+function calcularMedia(turma) {
+    return (turma[0].nota + turma[1].nota +turma[2].nota)/3
 }
+
+function enviaMsg(media, nomeTurma) {
+    if (media > 5) {
+        console.log(`A media da ${nomeTurma} foi de ${media.toFixed(1)} Parabens!.`)
+    } else {
+        console.log(`A media da turma ${nomeTurma} foi Menor que 5`)
+    }
+}
+      
