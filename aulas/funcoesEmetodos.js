03a444b646e0cdd25873cc4d825f23fbdd14f946
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-04 15:50:32 -0400

    Create funcoesEmetodos.js

---
+++ aulas/funcoesEmetodos.js
@@ -0,0 +1,21 @@
+// Criar um programa que Calcula a média 
+// da Turma de alunos e envia 
+// Mensagem de calculo da média
+
+const alunosTurma1 = [
+    {nome: 'Cleiton', nota: 10},
+    {nome:'Diego', nota:9.8},
+    {nome:'Fulano', nota:5}
+    
+]
+
+const alunosTurma2 = [
+    {nome: 'Robson', nota: 10},
+    {nome:'Lucy', nota:9.8},
+    {nome:'Ciclano', nota:5}
+    
+]
+
+function calcularMedia(alunos) {
+    const media = (alunosTurma1[0].nota + alunosTurma1[1].nota + alunosTurma1[2].nota)/3
+}
