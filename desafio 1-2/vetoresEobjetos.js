7e15966d3b6cacaa800830e8261b5e8d61267d24
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-04 14:44:02 -0400

    Update vetoresEobjetos.js

--- desafio 1-2/vetoresEobjetos.js
+++ desafio 1-2/vetoresEobjetos.js
@@ -1,19 +1,4 @@
-/*Vetores e objetos
-    Crie um programa com um objeto para armazenar dados de um programador como nome, idade e tecnologias que trabalha.
-    Um programador pode trabalhar com várias tecnologias, por isso armazene essas tecnologias em um array.
-    As tecnologias também devem ser objetos contendo nome e especialidade, use as tecnologias abaixo:
 
-{ nome: 'C++', especialidade: 'Desktop' }
-{ nome: 'Python', especialidade: 'Data Science' }
-{ nome: 'JavaScript', especialidade: 'Web/Mobile' }
-Por exemplo:
-
-const objeto = {
-  propriedade: [
-    { nome: "C++", especialidade: "Desktop" },
-    { nome: "JavaScript", especialidade: "Web/Mobile" }
-  ]
-}; */
 const programador = {nome:'Jonas',
                      idade: 18,
                      tecnologias:[{nome:'PHP', especialidade:'Deaktop'},
