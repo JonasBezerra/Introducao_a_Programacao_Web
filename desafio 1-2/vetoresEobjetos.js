4292ec2d4ca14b4d5b79d62cd9d4da37c7c46c43
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-04 14:50:02 -0400

    Update vetoresEobjetos.js

--- desafio 1-2/vetoresEobjetos.js
+++ desafio 1-2/vetoresEobjetos.js
@@ -1,11 +1,16 @@
 
+// OBJETOS INTERCALADOS. PODERIA FAZER UM OBJETO APENAS MAIS INTERCALO ELE PARA SIMPLIFICAR.
 const programador = {nome:'Jonas',
                      idade: 18,
-                     tecnologias:[{nome:'PHP', especialidade:'Deaktop'},
+                     tecnologias:[{nome:'PHP', especialidade:'Deaktop'}, 
                                   {nome:'Python', especialidade:'Data Science'},
                                   {nome:'Java', especialidade:'Mobile/Desktop'}]
                      }
 
-const tecnologias = [{nome:'C++'}, {especialidade:'Deaktop'},
+console.log(`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia`)                     
+                     
+/*const tecnologias = [{nome:'C++'}, {especialidade:'Deaktop'},
                      {nome:'Python',especialidade:'Data science'},
-                     {nome:'Java',especialidade:'Mobile/Desktop'}]                    
+                     {nome:'Java',especialidade:'Mobile/Desktop'}] 
+                     
+                     */                 
