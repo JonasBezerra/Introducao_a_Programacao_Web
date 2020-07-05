0437bdc9cfbdd2f54b0eceb3592f67ead292de15
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-05 11:00:55 -0400

    Create usuariosEtecnologia.js

---
+++ Desafio 1-3/usuariosEtecnologia.js
@@ -0,0 +1,15 @@
+/*
+Desafios para fortalecer alguns conceitos, entre eles:
+    Funções e métodos;
+    Estruturas de repetição;
+    Escopos.
+*/
+
+// Crie um programa que armazena um array de usuários (objetos), cada usuário tem um nome e suas tecnologias (novo array), por exemplo:
+
+
+var usuarios = [
+    {nome:'Jonas', tecnologias: ['Html','Css']},
+    {nome:'Luiz', tecnologias: ['Js','Node']},
+    {nome:'Pedro', tecnologias: ['Android', 'Java']}
+]
