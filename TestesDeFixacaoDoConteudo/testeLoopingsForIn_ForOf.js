5634796353c94d26e5e569ea9287268ddb7a76cc
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-08 21:27:16 -0400

    Update testeLoopingsForIn_ForOf.js

--- TestesDeFixacaoDoConteudo/testeLoopingsForIn_ForOf.js
+++ TestesDeFixacaoDoConteudo/testeLoopingsForIn_ForOf.js
@@ -5,17 +5,22 @@ const alunos = [
     console.log('\n==================================\n')
     
 
-    console.log(alunos[0].tecnologias)
     // primeiro fazer um for para o Objeto alunos
 
     function buscarCSS(alunos){ 
         for (const i of alunos.tecnologias) { // FOR OF ITERA SOBRE O VALOR DE CADA PROPRIEDADE.
-            console.log(i);
+            if(i ==='CSS'){
+                return true;
+            }
+        
         }
+        return false
     }
 
     // Agora fazer um outro looping dentro do Ojeto tecnologias Interno do Objeto alunos
     
-    for (const i of alunos) {
-        buscarCSS(i);
+    for (let i = 0; i < alunos.length ; i++) {
+        if(buscarCSS(alunos[i]) == true){
+            console.log(`O aluno ${alunos[i].nome} usa CSS`)
+        }
     }
