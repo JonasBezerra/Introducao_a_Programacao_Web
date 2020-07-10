b595047e9605040e929f5092fdb19df98f27966d
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-10 13:13:31 -0400

    arrayParametro

--- TestesDeFixacaoDoConteudo/desvendandoArrayDeObjetos.js
+++ TestesDeFixacaoDoConteudo/desvendandoArrayDeObjetos.js
@@ -1,7 +1,7 @@
 pessoas = [{nome:'Jonas',idade: 17}, {nome: 'Higor', idade:22}]
 
 
-for (let interator of pessoas) { // FOR OF INTERA SOBRE O ATRIBUTO DO OBJETO
+for (let interator of pessoas) { // FOR OF ITERA SOBRE O VALOR DO ATRIBUTO DO OBJETO
 
     console.log(interator) // AQUI ELE TRAS O VALOR DE TODOS OS OBJETOS DENTRO DO ARRAY
     console.log(interator.nome); // AQUI ELE TRAS APENAS O NOME DE TODOS OS OBJETOS
