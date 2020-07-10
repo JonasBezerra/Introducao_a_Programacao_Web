27e3921d1f6848c63260cabc87588d81f33a7f0e
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-10 13:07:46 -0400

    Update desvendandoArrayDeObjetos.js

--- TestesDeFixacaoDoConteudo/desvendandoArrayDeObjetos.js
+++ TestesDeFixacaoDoConteudo/desvendandoArrayDeObjetos.js
@@ -5,4 +5,22 @@ for (let interator of pessoas) { // FOR OF INTERA SOBRE O ATRIBUTO DO OBJETO
 
     console.log(interator) // AQUI ELE TRAS O VALOR DE TODOS OS OBJETOS DENTRO DO ARRAY
     console.log(interator.nome); // AQUI ELE TRAS APENAS O NOME DE TODOS OS OBJETOS
+
+    if(interator.idade >=18){
+        console.log(`\n ====== O aluno ${interator.nome} é maior de idade ====== `)
+    }
+}
+
+// VAMOS PEGAR TUDO ISSO E TRABALHAR COM FUNÇÕES
+console.log('\n\t======================\n')
+verificarPessoas(pessoas)
+
+function verificarPessoas(pessoa){
+    let idade = ''
+    for (let i = 0; i < pessoa.length; i++) {
+        idade = pessoa[i].idade;
+        if(idade < 18 ){
+            console.log(`O aluno ${pessoa[i].nome} é Menor de idade`)
+        }
+    }
 }
