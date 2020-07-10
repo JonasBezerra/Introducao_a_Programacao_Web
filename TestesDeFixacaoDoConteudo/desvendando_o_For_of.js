9e8b64466d4edff6736745c70303e6ecc8ca0dae
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-10 10:09:22 -0400

    desvendandoOforOf

--- TestesDeFixacaoDoConteudo/desvendando_o_For_of.js
+++ TestesDeFixacaoDoConteudo/desvendando_o_For_of.js
@@ -10,4 +10,5 @@ for (const element of array1) {
 
 /* O (para... de) instrução cria um loop iterating sobre objetos iteráveis, incluindo: Stringembutido , Array, objetos semelhantes a matriz (por exemplo, argumentos ou NodeList), TypedArray, Map, Sete iterables definidos pelo usuário. Ele invoca um gancho de iteração personalizado com declarações a serem executadas pelo valor de cada propriedade distinta do objeto.*/
 
-/*RESUMINDO ELE ITERA SOBRE OS VALORES DE CADA ATRIBUTO DENTRO DO OBJETO */
+/*RESUMINDO ELE ITERA SOBRE OS VALORES DE CADA ATRIBUTO DENTR
+ DO OBJETO */
