f76ff7e0d3991c939fe2544a07a78e1c2af392c1
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-18 14:05:45 -0400

    Update ex_1-3.js

--- todosDesafiosResolvidos/ex_1-3.js
+++ todosDesafiosResolvidos/ex_1-3.js
@@ -13,10 +13,41 @@ const users = [
 // todo array [] recebe métodos e atributos nativos.
 // length para saber qntos elementos tem no meu array
 
-for (let i = 0; i < users.length; i++) {
+
+// ================================ ESSA FOI A FORMA MAIS FÁCIL ENCONTRADA ======================
+
+/*for (let i = 0; i < users.length; i++) {
     console.log(`${users[i].name} Trabalha com ${users[i].tecnology}`)
+     o atributo name não é um atributo do array users e sim de um objeto dentro do array
+}*/
+
+// ===========================================================================================================
+ 
+/* 
+for(let i = 0; i < users.length ; i++){ // PERCORRE O PRIMEIRO ARRAY
+    let tecnologyes = ''
+    for(let a = 0; a < users[i].tecnology.length; a++){  // PERCORRES O SEGUNDO ARRAY. a SO PODE SER ACESSADO DENTRO DO LOOPING
+        if(a==0){
+            tecnologyes += ""+users[i].tecnology[a]
+        }else{
+            tecnologyes += ","+users[i].tecnology[a]
+        }
+    }
+    console.log(`${users[i].name} trabalha com ${tecnologyes}`)
+}
+*/
+
+
+// =====================================================================================
+
+for (let user of users) {// pega a 1 pos, depois a seg até acabar o looping
+    console.log(`${user.name} trabalha com ${user.tecnology}`)
 }
 
+// =====================================================================================
+
+
 // ATENÇÃO: SE O ERRO FOR DIZENDO QUE UMA VARIAVEL NÃO ESTA DEFINIDA ISSO QUE DIZER QUE ELA NÃO EXISTE
+// SintaxError: QUER DIZER QUE EU ESCREVÍ ALGUMA COISA ERRADA
 
 // PALAVRAS COM MESMO SIGNIFICADO: ITERAR SOBRE UM ARRAY MESMO QUE VARRER O ARRAY, PERCORRER O ARRAY, ANDAR SOBRE O ARRAY OU COLEÇÃO 
