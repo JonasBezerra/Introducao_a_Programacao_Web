29b53083f54319608a75ead242e6abd7d4c9bd6f
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-18 06:22:58 -0400

    Create organizacaoPadronizacaoEscrita.js

---
+++ aulas/organizacaoPadronizacaoEscrita.js
@@ -0,0 +1,34 @@
+// ============== ORGANIZAÇÃO, PADRONIZAÇÃO E ESCRITA =========
+
+const alunos = [
+    {nome: 'Jonas', nota: 9.8 },
+    {nome: 'Gustavo', nota: 8.0},
+    {nome: 'Maik', nota: 4 }
+]
+
+function marcarReprovado(alunos){
+    alunos.reprovado = false // coloca um atributo a mas em cada objeto
+    
+        if(alunos.nota < 5){
+            alunos.reprovado = true
+    }
+    console.table(alunos)
+}
+
+function enviarMsgReprovado(alunos){
+    // SE A MÉDIA FOR < 5
+    if(alunos.reprovado){
+        console.log(`O aluno ${alunos.nome} está reprovado!.`)
+    }
+}
+
+function alunoReprovado(alunos){
+    for (let aluno of alunos) {
+        marcarReprovado(aluno)
+        enviarMsgReprovado(aluno)
+    }
+  
+}
+
+alunoReprovado(alunos)
+
