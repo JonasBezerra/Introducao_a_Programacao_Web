830c7b80946b519c4469ea077a89a86d3c99eaa8
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-18 07:47:25 -0400

    Update operacaoBancaria.js

--- Desafio_1-4/operacaoBancaria.js
+++ Desafio_1-4/operacaoBancaria.js
@@ -2,3 +2,12 @@ const user = {
     name: 'Maria',
     transactio: [],
     balance:0}
+
+function createTransactio(user) {
+    user.type = 'Credit'
+    
+
+    for (const us of user) {
+        
+    }
+}
