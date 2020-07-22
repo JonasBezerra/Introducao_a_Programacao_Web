4716b98e4a5d77939a9e43e9785fd2564f41fc21
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-22 17:08:03 -0400

    Update buscaPorTecnologia.js

--- todosDesafiosResolvidos/desafio-1-3/buscaPorTecnologia.js
+++ todosDesafiosResolvidos/desafio-1-3/buscaPorTecnologia.js
@@ -10,26 +10,29 @@ function checaSeUsuarioUsaCSS(user) {
     // percorra o array de tecnologias do usuário até encontrar se ele trabalha com CSS
     // SE encontrar, retorne true da função, caso contrário retorne false
     for (let tecnology  of user.tecnologyes) {
-        if(tecnology == 'CSS'){
-            return true
-        }else{
-            return false
+        const encontriTecnologiaCSS = tecnology == 'CSS'
+        if(encontriTecnologiaCSS){
+            return true; // se encontra o return ela para a função
         }
     }
+    return false
 }
-
 for (let poss in users) {
-    let usaCSS = checaSeUsuarioUsaCSS(users[poss])
-    console.log(usaCSS);
+    let encontradoCSS = checaSeUsuarioUsaCSS(users[poss])
+    
+    if(encontradoCSS){
+        console.log(`O usuário ${users[poss].name} usa CSS`)
+    }
 }
 
 // 5 coisas que vc PRECISA saber sobre funções (iniciante)
 
-// 1 of 5 - Escrever uma função / criar - 20%
-// 2 of 5 - Saber enviar dado pra dentro da função (parâmetro) - 20%
-// 3 of 5 - Saber como enviar dados para fora da função ou retornar- 20%
-// 4 of 5 - Saber como utilizar / executar ou chamar a função - 20%
-// 5 of 5 - Saber como enviar os dados para dentro da função qndo eu chamar ela
+// 1 of 6 - Escrever uma função / criar - 20%
+// 2 of 6 - Saber enviar dado pra dentro da função (parâmetro) - 20%
+// 3 of 6 - Saber como enviar dados para fora da função ou retornar- 20%
+// 4 of 6 - Saber como utilizar / executar ou chamar a função - 20%
+// 5 of 6 - Saber como enviar os dados para dentro da função qndo eu chamar ela
+// 6 of 6 - Saber como capturar o dado de dentro para fora
 
 // Curiosidade extra que vc DEVERIA estar sabendo sobre função / Método
 
@@ -37,3 +40,5 @@ for (let poss in users) {
     // para reutilizar um bloco de código
 // Qual o objetivo?.
     // para eu não repetir o mesmo código
+
+// A função não precisa Obrigatóriamente  retornar valores
