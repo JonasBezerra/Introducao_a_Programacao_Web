99506f645c5cd087e3cad54d3eb9b69561728f7f
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-22 15:32:47 -0400

    Update testandoJoin.js

--- TestesDeFixacaoDoConteudo/testandoJoin.js
+++ TestesDeFixacaoDoConteudo/testandoJoin.js
@@ -3,5 +3,31 @@ let pessoas = [
     {nome: 'Jhoney', idade: 21},
     {nome: 'Fulano', idade: 12}
 ]
+    /*O método join() cria e devolve uma string ao concatenar todos os elementos numa array (ou objeto como um vetor / matriz) numa cadeia separada por vírgulas ou outro separador especificado. Se a array tem um só item, esse item é devolvido sem o uso do separador.*/
 
-    console.log(pessoas.join(', '));
+let rJ = pessoas.join(', ');
+console.log(typeof rJ) // Retorna uma string
+
+// ========== EXEMPLO TWO =======
+const elements = ['Fire', 'Air', 'Water'];
+
+console.log(elements.join());
+// expected output: "Fire,Air,Water"
+
+console.log(elements.join(''));
+// expected output: "FireAirWater"
+
+console.log(elements.join('-'));
+// expected output: "Fire-Air-Water"
+
+// =================== O parametro ======================
+
+/*Especifica uma string (cadeia de caracteres) para separar cada elemento do vetor. O separador é convertido em cadeia se for necessário. Se for omitido, os elementos serão separados por virgulas (","). Se separador é uma cadeia vazia, todos os elementos são unidos sem qualquer caráter entre eles.*/
+
+// ================= EXEMPLO TREE ========
+
+var a = ['Vento', 'Chuva', 'Fogo'];
+a.join();      // 'Vento,Chuva,Fogo'
+a.join(', ');  // 'Vento, Chuva, Fogo'
+a.join(' + '); // 'Vento + Chuva + Fogo'
+a.join('');    // 'VentoChuvaFogo'
