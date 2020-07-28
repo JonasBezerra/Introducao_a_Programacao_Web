0fb04dc8c79bae954cbef8ce27f132a1d753f877
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-07-27 20:40:23 -0400

    Update operacaoBancaria.js

--- Desafio_1-4/operacaoBancaria.js
+++ Desafio_1-4/operacaoBancaria.js
@@ -5,12 +5,27 @@ const user = {
 }
 
 function createTransaction(transaction) {
+    user.transactions.push(transaction)
     if(transaction.type == 'Credit'){
         user.balance += transaction.value;
     }else{
         user.balance -= transaction.value
     }
+
 }
+
 const transaction = {
     type: 'Credit', value: 50.5
 }
+
+// calling creatTransaction()
+
+createTransaction(transaction);
+console.log(user)
+
+const transaction2 = {
+    type: 'Debit', value:100.5
+}
+
+createTransaction(transaction2);
+console.log(user)
